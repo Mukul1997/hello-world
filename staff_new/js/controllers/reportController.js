c2c616fdf73080cdd11d667a96c953f136040697
Mukul1997 <mukul_hs@live.com>
2017-07-28 19:56:02 +0530

    staff new

---
+++ staff_new/js/controllers/reportController.js
@@ -0,0 +1,47 @@
+angular.module('myApp').controller('reportController', function($scope,$http,LoginFactory) {
+    $scope.staff_id=LoginFactory.getID;
+    $scope.loadSemester = function(){  
+           $http.post("pages/php/semester_attendance.php",{'staffid':$scope.staff_id}).then(function(response){  
+                $scope.semester = response.data;
+           });
+
+        }
+
+        $scope.loadSubject = function(){  
+           $http.get("pages/php/retrieve1.php").then(function(response){  
+                $scope.subject = response.data;
+           });
+
+        }  
+        
+        $scope.loadSection = function(){  
+             $http.post("pages/php/retrieve2.php").then(function(response){  
+                  $scope.section = response.data;  
+             }); 
+        }
+
+        $scope.attendanceView = function(){
+              $scope.date1 = document.getElementById('x').value;
+              $scope.date2 = document.getElementById('y').value;
+              console.log($scope.date1);
+              console.log($scope.date2);
+           $http({  
+                     method:'POST',
+                     url:'pages/php/report_attendance.php',  
+                     data:{'selectedFromDate':$scope.date1,
+                          'selectedToDate':$scope.date2,
+                          'sem_attend':$scope.sem_attend,
+                          'sub_attend':$scope.sub_attend,
+                          'sec_attend':$scope.sec_attend,
+                          'staffid':$scope.staff_id
+                      		}
+              }).then(function(response){
+             $scope.StudAtt = response.data;
+               console.log(response.data);  
+              $scope.usn=$scope.StudAtt[0];
+              $scope.dates=$scope.StudAtt[1];
+             });
+            
+          }
+          $scope.loadSemester();
+});
